1439a8466d41cea7b3af70887d9761aa0de6899e
may28js <may28js1@126.com>
2026-01-23 14:26:24 +0800

    chore: Remove sensitive maintenance scripts and update gitignore

--- WebBackend/Client/extract_layout.py
+++
@@ -1,7 +0,0 @@
-
-file_path = r'F:\工作区\模块开发\StoryOfTimeLauncher\WebBackend\demo页面源代码\Home _ Next Vision CMS_buy.html'
-
-with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
-    f.seek(485400)
-    content = f.read(5000)
-    print(content)
